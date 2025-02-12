c6ef23cc93b3ce6b32121b37cf6f7dda4cd2670e
bruno-beloff-aviva <bruno.beloff@aviva.com>
2025-02-12 14:26:42 +0000

    Altered reporting

--- service/subscribe_service.go
+++ service/subscribe_service.go
@@ -41,26 +41,26 @@ func (m SubscribeService) Receive(ctx context.Context, record events.SQSMessage)
 		return err
 	}
 
-	m.logger.Info("Receive: ", zap.Any("message", message))
+	m.logger.Debug("Receive: ", zap.Any("message", message))
 
 	if strings.Contains(message.Path, "sleep") {
 		// sleep...
-		m.logger.Warn("*** SLEEP: ", zap.Any("message", message.Path))
+		m.logger.Warn("*** SLEEP: ", zap.Any("Path", message.Path))
 		time.Sleep(sleepSeconds * time.Second)
 
 	} else if strings.Contains(message.Path, "error") {
 		// error...
-		m.logger.Warn("*** ERROR: ", zap.Any("message", message.Path))
+		m.logger.Warn("*** ERROR: ", zap.Any("Path", message.Path))
 		return errors.New(message.Path)
 
 	} else if strings.Contains(message.Path, "panic") {
 		// panic...
-		m.logger.Warn("*** PANIC: ", zap.Any("message", message.Path))
+		m.logger.Warn("*** PANIC: ", zap.Any("Path", message.Path))
 		panic(message.Path)
 
 	} else {
 		// ok...
-		m.logger.Warn("*** OK: ", zap.Any("message", message.Path))
+		m.logger.Warn("*** OK: ", zap.Any("Path", message.Path))
 	}
 
 	// dbManager.Put...
