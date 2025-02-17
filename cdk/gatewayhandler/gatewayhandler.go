e1c11322b59aef7b9588aab2cde54ba99424c0cc
bruno-beloff-aviva <bruno.beloff@aviva.com>
2025-02-17 16:31:01 +0000

    Fixed

--- cdk/gatewayhandler/gatewayhandler.go
+++ cdk/gatewayhandler/gatewayhandler.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"sqstest/cdk/dashboard"
 
-	"github.com/aws/aws-cdk-go/awscdk/awscloudwatch"
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awscloudwatch"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslogs"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awssns"
